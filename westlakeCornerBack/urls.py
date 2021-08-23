efa245364dc9aa3a41213237e276e2f3c79e377c
KalionCode <xxfaya.dragonxx@gmail.com>
2021-08-24 11:36:16 +1200

    Edited index.html

--- westlakeCornerBack/urls.py
+++ westlakeCornerBack/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/', include('api.urls')),
+        path('admin/', admin.site.urls),
     path('', include('app.urls')),
-    path('admin/', admin.site.urls)
 ]
