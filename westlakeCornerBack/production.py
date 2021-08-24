29efed6870cd284fdea54bb8bba117b4965d8fb3
KalionCode <xxfaya.dragonxx@gmail.com>
2021-08-24 21:04:31 +1200

    Fixed production settings

--- westlakeCornerBack/production.py
+++ westlakeCornerBack/production.py
@@ -5,6 +5,7 @@ import os
 # that Azure automatically creates for us.
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
+DEBUG = True
 # WhiteNoise configuration
 MIDDLEWARE = [                                                                   
     'django.middleware.security.SecurityMiddleware',
@@ -19,19 +20,4 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-""" # DBHOST is only the server name, not the full URL
-hostname = os.environ['DBHOST']
-
-# Configure Postgres database; the full username is username@servername,
-# which we construct using the DBHOST value.
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DBNAME'],
-        'HOST': hostname + ".postgres.database.azure.com",
-        'USER': os.environ['DBUSER'] + "@" + hostname,
-        'PASSWORD': os.environ['DBPASS'] 
-    }
-} """
+STATIC_ROOT = BASE_DIR / 'staticfiles'
