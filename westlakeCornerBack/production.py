02f40c504f70f6d6350977d3e30e8560706855e1
KalionCode <xxfaya.dragonxx@gmail.com>
2021-08-24 21:04:31 +1200

    Fixed production settings

--- westlakeCornerBack/production.py
+++ westlakeCornerBack/production.py
@@ -5,6 +5,7 @@ import os
 # that Azure automatically creates for us.
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
+DEBUG = False
 # WhiteNoise configuration
 MIDDLEWARE = [                                                                   
     'django.middleware.security.SecurityMiddleware',
